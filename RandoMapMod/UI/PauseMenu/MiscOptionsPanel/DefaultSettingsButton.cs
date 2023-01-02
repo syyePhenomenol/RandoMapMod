41563dd88b5136d0cdeb8986242a363603d48759
syyePhenomenol <yye3028@gmail.com>
2023-01-02 17:47:00 +1100

    Add default settings reset

---
+++ RandoMapMod/UI/PauseMenu/MiscOptionsPanel/DefaultSettingsButton.cs
@@ -0,0 +1,36 @@
+﻿using MagicUI.Elements;
+using MapChanger.UI;
+using RandoMapMod.Settings;
+using L = RandomizerMod.Localization;
+
+namespace RandoMapMod.UI
+{
+    internal class DefaultSettingsButton : ExtraButton
+    {
+        public DefaultSettingsButton() : base(nameof(DefaultSettingsButton), RandoMapMod.MOD)
+        {
+
+        }
+
+        public override void Make()
+        {
+            base.Make();
+
+            Button.Borderless = true;
+        }
+
+        protected override void OnClick()
+        {
+            GlobalSettings.ResetToDefaultSettings();
+        }
+
+        public override void Update()
+        {
+            string text = $"{L.Localize("Reset global\nsettings")}";
+
+            Button.ContentColor = RmmColors.GetColor(RmmColorSetting.UI_Special);
+
+            Button.Content = text;
+        }
+    }
+}
