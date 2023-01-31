39bfa91d9653be1d004382b3f592bfe4e659f14b
syyePhenomenol <yye3028@gmail.com>
2023-01-31 18:00:53 +1100

    Fix NRE with BenchRando interop

--- RandoMapMod/RandoMapMod.cs
+++ RandoMapMod/RandoMapMod.cs
@@ -90,7 +90,7 @@ namespace RandoMapMod
             Instance = this;
         }
 
-        public override string GetVersion() => "3.1.3";
+        public override string GetVersion() => "3.1.4";
 
         public override int LoadPriority() => 10;
 
