20c4f66f05c0fc83afd2c0562dd0dccb7232f111
syyePhenomenol <yye3028@gmail.com>
2023-04-04 15:50:44 +1000

    Bump version

--- RandoMapMod/RandoMapMod.cs
+++ RandoMapMod/RandoMapMod.cs
@@ -91,7 +91,7 @@ namespace RandoMapMod
             Instance = this;
         }
 
-        public override string GetVersion() => "3.2.2";
+        public override string GetVersion() => "3.3.0";
 
         public override int LoadPriority() => 10;
 
