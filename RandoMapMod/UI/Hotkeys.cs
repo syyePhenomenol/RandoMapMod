3939fd5ee9e74c203d8890283ad9f223b6ae757b
syyePhenomenol <85815578+syyePhenomenol@users.noreply.github.com>
2024-04-19 09:24:29 +0200

    Fix hotkey active condition

--- RandoMapMod/UI/Hotkeys.cs
+++ RandoMapMod/UI/Hotkeys.cs
@@ -6,6 +6,7 @@ using RandoMapMod.Pathfinder;
 using RandoMapMod.Rooms;
 using UnityEngine;
 using MapChanger;
+using SN = ItemChanger.SceneNames;
 
 namespace RandoMapMod.UI
 {
@@ -22,26 +23,26 @@ namespace RandoMapMod.UI
             {
                 RandoMapMod.GS.ToggleControlPanel();
                 MapUILayerUpdater.Update();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.K, () =>
             {
                 RandoMapMod.GS.ToggleMapKey();
                 MapUILayerUpdater.Update();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.P, () =>
             {
                 RandoMapMod.GS.TogglePinSelection();
                 UpdateSelectors();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.C, () =>
             {
                 RandoMapMod.GS.ToggleShowItemCompass();
                 ItemCompass.Update();
                 MapUILayerUpdater.Update();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             if (Interop.HasBenchwarp())
             {
@@ -50,20 +51,20 @@ namespace RandoMapMod.UI
                     RandoMapMod.GS.ToggleBenchwarpPins();
                     RmmPinManager.MainUpdate();
                     UpdateSelectors();
-                }, ModifierKeys.Ctrl, () => MapChanger.Settings.MapModEnabled());
+                }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
             }
 
             Root.ListenForHotkey(KeyCode.R, () =>
             {
                 RandoMapMod.GS.ToggleRoomSelection();
                 UpdateSelectors();
-            }, ModifierKeys.Ctrl, () => Conditions.TransitionRandoModeEnabled());
+            }, ModifierKeys.Ctrl, () => GlobalHotkeyCondition() && Conditions.TransitionRandoModeEnabled());
 
             Root.ListenForHotkey(KeyCode.S, () =>
             {
                 RandoMapMod.GS.ToggleShowReticle();
                 UpdateSelectors();
-            }, ModifierKeys.Ctrl, () => MapChanger.Settings.MapModEnabled());
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             if (Interop.HasBenchwarp())
             {
@@ -73,59 +74,83 @@ namespace RandoMapMod.UI
                     RouteManager.ResetRoute();
                     MapUILayerUpdater.Update();
                     RouteCompass.Update();
-                }, ModifierKeys.Ctrl, () => Conditions.TransitionRandoModeEnabled());
+                }, ModifierKeys.Ctrl, () => GlobalHotkeyCondition() && Conditions.TransitionRandoModeEnabled());
             }
 
             Root.ListenForHotkey(KeyCode.G, () =>
             {
                 RandoMapMod.GS.ToggleProgressHint();
                 MapUILayerUpdater.Update();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForPlayerAction(InputHandler.Instance.inputActions.superDash, () =>
             {
                 ProgressHintPanel.Instance.RevealProgressHint();
-            }, () => States.WorldMapOpen && RandoMapMod.GS.ProgressHint is not Settings.ProgressHintSetting.Off && NoCtrl());
+            }, () => GlobalHotkeyCondition() && States.WorldMapOpen && RandoMapMod.GS.ProgressHint is not Settings.ProgressHintSetting.Off && NoCtrl());
 
             Root.ListenForHotkey(KeyCode.Alpha1, () =>
             {
                 RandoMapMod.LS.ToggleSpoilers();
                 UpdatePins();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.Alpha2, () =>
             {
                 RandoMapMod.LS.ToggleRandomized();
                 UpdatePins();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.Alpha3, () =>
             {
                 RandoMapMod.LS.ToggleVanilla();
                 UpdatePins();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.Alpha4, () =>
             {
                 RandoMapMod.GS.TogglePinShape();
                 UpdatePins();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.Alpha5, () =>
             {
                 RandoMapMod.GS.TogglePinSize();
                 UpdatePins();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
 
             Root.ListenForHotkey(KeyCode.D, () =>
             {
                 Debugger.LogMapPosition();
-            }, ModifierKeys.Ctrl);
+            }, ModifierKeys.Ctrl, GlobalHotkeyCondition);
         }
 
+        private HashSet<string> nonGameplayScenes = new()
+        {
+            SN.Cinematic_Ending_A,
+            SN.Cinematic_Ending_B,
+            SN.Cinematic_Ending_C,
+            SN.Cinematic_Ending_E,
+            SN.Cinematic_MrMushroom,
+            SN.Cinematic_Stag_travel,
+            SN.Cutscene_Boss_Door,
+            SN.End_Credits,
+            SN.End_Game_Completion,
+            SN.Menu_Credits,
+            SN.Menu_Title,
+            SN.Opening_Sequence,
+            SN.PermaDeath,
+            SN.PermaDeath_Unlock
+        };
+
+        private bool GlobalHotkeyCondition()
+        {
+            return Conditions.RandoMapModEnabled() && !nonGameplayScenes.Contains(GameManager.instance.sceneName);
+        }
+
+        // This doesn't affect the hotkeys
         protected override bool Condition()
         {
-            return Conditions.RandoMapModEnabled();
+            return true;
         }
 
         private void UpdateSelectors()
